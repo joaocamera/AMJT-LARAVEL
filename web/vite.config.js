f75f359a723353622cd19815751e8a936c5e911c
joaocamera <jcamera@gmail.com>
2025-12-30 14:21:27 -0300

    Allow hosts for Vite dev server

--- web/vite.config.js
+++ web/vite.config.js
@@ -2,5 +2,8 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  server: {
+    allowedHosts: ["amjt2.com.br", "api.amjt2.com.br"]
+  }
 });
